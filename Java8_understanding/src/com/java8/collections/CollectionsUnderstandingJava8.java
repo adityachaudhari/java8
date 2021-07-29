0d9f5a81a36aa6602bff8d4a4859674989f34664
adityachaudhari <chaudhari.aditya24@gmail.com>
2021-07-29 10:19:01 +0530

    some partial work commit

---
+++ Java8_understanding/src/com/java8/collections/CollectionsUnderstandingJava8.java
@@ -0,0 +1,12 @@
+package com.java8.collections;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class CollectionsUnderstandingJava8 {
+
+    public static void main(String[] args) {
+
+        //Collectors
+    }
+}
